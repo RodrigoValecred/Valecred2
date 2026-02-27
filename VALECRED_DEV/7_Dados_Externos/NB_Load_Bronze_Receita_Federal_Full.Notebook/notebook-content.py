42a13355e636e8c766ee9bd70db4bf1892c83438
RodrigoVC <rodrigo@valecred.com.br>
2026-02-27 16:31:14 -0300

    Committing 1 item from workspace 41ae19db-f71d-471f-9ac7-ccbc2c75ce11

--- VALECRED_DEV/7_Dados_Externos/NB_Load_Bronze_Receita_Federal_Full.Notebook/notebook-content.py
+++ VALECRED_DEV/7_Dados_Externos/NB_Load_Bronze_Receita_Federal_Full.Notebook/notebook-content.py
@@ -112,6 +112,13 @@ schema_estabelecimentos = StructType([
     StructField("data_situacao_especial", StringType(), True)
 ])
 
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
 # CELL ********************
 
 def safe_extract(zip_ref, path):
@@ -228,6 +235,13 @@ def download_and_extract(filename, base_dir_download, base_dir_extract):
         print(f"FALHA FATAL: Não foi possível baixar {filename} de nenhum mirror.")
         return False
 
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
 # CELL ********************
 
 # --- Execução Download e Extração ---
@@ -252,6 +266,13 @@ try:
         print(f" - {f.name}")
 except Exception as e:
     print(f"Erro ao listar arquivos extraídos: {e}")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
 
 # CELL ********************
 
@@ -284,6 +305,13 @@ try:
 
 except Exception as e:
     print(f"Erro ao processar EMPRESAS: {e}")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
 
 # CELL ********************
 
