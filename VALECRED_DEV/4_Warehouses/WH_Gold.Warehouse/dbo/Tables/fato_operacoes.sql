912107f3da3d394e899e41e7a798e4a196495c70
RodrigoVC <rodrigo@valecred.com.br>
2026-03-03 15:43:44 -0300

    Committing 1 item from workspace 41ae19db-f71d-471f-9ac7-ccbc2c75ce11

--- VALECRED_DEV/4_Warehouses/WH_Gold.Warehouse/dbo/Tables/fato_operacoes.sql
+++ VALECRED_DEV/4_Warehouses/WH_Gold.Warehouse/dbo/Tables/fato_operacoes.sql
@@ -11,11 +11,10 @@ CREATE TABLE [dbo].[fato_operacoes] (
 	[valor_x_prazo] float NULL, 
 	[chave_produto] varchar(4) NULL, 
 	[tac_mesa] decimal(18,2) NULL, 
-	[tarifas_de_operacao] decimal(18,2) NULL
-,
-	[floating] float NULL,
-	[prazo_medio] float NULL,
-	[prazo_medio_total] float NULL,
-	[menor_vencimento] date NULL,
+	[tarifas_de_operacao] decimal(18,2) NULL, 
+	[floating] float NULL, 
+	[prazo_medio] float NULL, 
+	[prazo_medio_total] float NULL, 
+	[menor_vencimento] date NULL, 
 	[maior_vencimento] date NULL
 );
