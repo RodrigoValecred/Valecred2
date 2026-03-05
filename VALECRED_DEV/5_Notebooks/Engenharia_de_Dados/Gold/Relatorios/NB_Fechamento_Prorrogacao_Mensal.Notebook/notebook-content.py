0b1fcd944b0844222327ccedbb0c8a1a87865588
RodrigoVC <rodrigo@valecred.com.br>
2026-03-05 10:32:19 -0300

    Committing 3 items from workspace 41ae19db-f71d-471f-9ac7-ccbc2c75ce11

--- VALECRED_DEV/5_Notebooks/Engenharia_de_Dados/Gold/Relatorios/NB_Fechamento_Prorrogacao_Mensal.Notebook/notebook-content.py
+++ VALECRED_DEV/5_Notebooks/Engenharia_de_Dados/Gold/Relatorios/NB_Fechamento_Prorrogacao_Mensal.Notebook/notebook-content.py
@@ -1,9 +1,23 @@
 # Fabric notebook source
 
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   }
+# META }
 
 # CELL ********************
 
 # Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
 
 # MARKDOWN ********************
 
