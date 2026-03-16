6f69c68aaa6a86a8fb39aca63f479044050857ac
RodrigoVC <rodrigo@valecred.com.br>
2026-03-16 13:55:22 -0300

    Committing 1 item from workspace 41ae19db-f71d-471f-9ac7-ccbc2c75ce11

--- VALECRED_DEV/7_Dados_Externos/Receita Federal/NB_Extract_Bronze_Receita_Federal_Full.Notebook/notebook-content.py
+++ VALECRED_DEV/7_Dados_Externos/Receita Federal/NB_Extract_Bronze_Receita_Federal_Full.Notebook/notebook-content.py
@@ -1,9 +1,35 @@
 # Fabric notebook source
 
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "553c2931-573b-4db0-838d-a70a01306d32",
+# META       "default_lakehouse_name": "LH_Bronze",
+# META       "default_lakehouse_workspace_id": "41ae19db-f71d-471f-9ac7-ccbc2c75ce11",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "553c2931-573b-4db0-838d-a70a01306d32"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
 
 # CELL ********************
 
 # Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
 
 # MARKDOWN ********************
 
