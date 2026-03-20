c8ce501e4538f77b6c5d0a041453f8887de6d9e6
RodrigoVC <rodrigo@valecred.com.br>
2026-03-20 09:18:44 -0300

    Committing 2 items from workspace 41ae19db-f71d-471f-9ac7-ccbc2c75ce11

--- VALECRED_DEV/5_Notebooks/Engenharia_de_Dados/Gold/NB_Gold_Carteira_Titulos.Notebook/notebook-content.py
+++ VALECRED_DEV/5_Notebooks/Engenharia_de_Dados/Gold/NB_Gold_Carteira_Titulos.Notebook/notebook-content.py
@@ -30,7 +30,7 @@
 # - status_analise = D
 # - aceito = S
 # - tipo_documento <> BL (usando t_doc em fato_titulos)
-# - liquidacao is not null
+# - liquidacao is null
 # 
 # **Tabelas Origem:** `LH_Gold.fato_operacoes`, `LH_Gold.fato_titulos`.
 # 
@@ -84,17 +84,37 @@ df_ops_filtered = df_ops.filter(
     (col("status_analise") == "D")
 ).select("cod_operacao", "status_aceite", "status_analise")
 
-# Filtro de Títulos: aceito = S, t_doc <> BL, liquidacao is not null
+# Filtro de Títulos: aceito = S, t_doc <> BL, liquidacao is null
 df_titulos_filtered = df_titulos.filter(
     (col("aceito") =="S") &
     (col("t_doc") != "BL") &
-    (col("liquidacao").isNotNull())
+    (col("liquidacao").isNull())
 )
 
 # Join para consolidar as regras
 df_carteira = df_titulos_filtered.join(df_ops_filtered, "cod_operacao", "inner")
 
 print(f"Titulos na carteira elegíveis: {df_carteira.count()}")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# ## Seção 2: Persistência
+
+# CELL ********************
+
+target_table = "LH_Gold.carteira_de_titulos"
+print(f"Salvando em {target_table}...")
+
+df_carteira.write.mode("overwrite").option("overwriteSchema","true").saveAsTable(target_table)
+
+print("Salvo com sucesso.")
 
 # METADATA ********************
 
